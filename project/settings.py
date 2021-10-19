63461a42c18e11eba6acb18978f226b319f0c3b9
JOFLIX <joflixooko@outlook.com>
2021-10-19 10:33:30 +0300

    resolving the account_user bug

--- project/settings.py
+++ project/settings.py
@@ -95,7 +95,7 @@ TEMPLATES = [
         },
     },
 ]
-
+# account_user
 WSGI_APPLICATION = 'project.wsgi.application'
 
 
@@ -104,8 +104,13 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME':'project',
+        'USER':'nepcollege',
+        'PASSWORD':'password',
+        'HOST':'',
+        'PORT':'',
     }
 }
 
